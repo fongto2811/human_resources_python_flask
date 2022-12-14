1e300f3c34eb89bcc48e7ab5fae20b2ddce22cf7
kaka2k1 <quyetphonga3@gmail.com>
2022-12-14 23:30:16 +0700

    Them profile

--- UI/routes.py
+++ UI/routes.py
@@ -1,6 +1,9 @@
 from UI import ui
 from flask import render_template, redirect
 
-@ui.route('/')
-def home():
+@ui.route('/users')
+def user():
     return render_template('users/index.html', page = 5)
+@ui.route('/profiles')
+def profile():
+    return render_template('profiles/index.html', page = 5)
