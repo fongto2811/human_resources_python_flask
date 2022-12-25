b04ed5a9e8ae580f336ad6aa74680f249347841a
fongto2811 <tothanhphong281101@gmail.com>
2022-12-25 21:47:15 +0700

    change config

--- run.py
+++ run.py
@@ -9,7 +9,10 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 from UI import setup_app
-app = setup_app()
+app = setup_app(
+    FLASK_ENV=os.environ["FLASK_ENV"],
+    FLASK_DEBUG=os.environ["FLASK_DEBUG"]
+)
 
 if __name__ == "__main__":
-    app.run(host=os.environ["HOST"],port=os.environ["PORT"],debug=os.environ["DEBUG"])
+    app.run(host=os.environ["HOST"], port=os.environ["PORT"])
