720c6d0cd099bae60eda0e014bf832f92f1efc83
fongto2811 <tothanhphong281101@gmail.com>
2022-12-25 20:16:16 +0700

    change folder

--- DAL/user.py
+++ DAL/user.py
@@ -1,12 +1,17 @@
-from cx_Oracle import connect, DatabaseError
+from cx_Oracle import connect, DatabaseError, makedsn, SYSDBA
 
+dsn_tns = makedsn('localhost','1521',service_name='orclpdb.localdomain')
 class UserDAL:
     def __init__(self, username, password) -> None:
         try:
-            conn = connect(user=username, password=password,host="localhost", port=1521, service_name="ORCLPDB.LOCALDOMAIN")
-            print(conn.version)
-        except DatabaseError as e:
-            print("There is a problem with Oracle", e)
+            conn = connect(user=username, password=password, dsn=dsn_tns, encoding="UTF-8")
+            cursor = conn.cursor()
+            sql = "select user from dual"
+            cursor.execute(sql)
+            for row in cursor:
+                print(row)
+        except DatabaseError as error:
+            print("There is a problem with Oracle", error)
 
 if __name__ == '__main__':
-    UserDAL('amy','amy')
+    UserDAL('phong','123')
--- UI/__init__.py
+++ UI/__init__.py
@@ -1,8 +1,25 @@
-'''
-Đăng ký các Blueprint templates
-'''
 from flask import Flask
+app = None
 
-ui = Flask(__name__)
+def setup_app(**config_overrides):
+    
+    global app
+    app = Flask(__name__)
 
-from UI import routes, models
+    # Load file config
+    app.config.from_object("app.config")
+    app.config.update(config_overrides)
+
+    # Định nghĩa các đường dẫn
+    register_path(app)
+    # Định nghĩa các BLL
+    register_bll(app)
+    
+    return app
+
+def register_path(app):
+    from UI.routers import login
+    app.register_blueprint(login)
+
+def register_bll(app):
+    pass
